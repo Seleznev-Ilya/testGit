1a6dadd36c33d2157424be3494c5ba99c119b3ef
Ilia Selieznov <ziziron21@gmail.com>
2021-03-23 07:01:18 +0200

    testing js

--- sctipt.js
+++ sctipt.js
@@ -1,2 +1,6 @@
-let myName = 'Ilya';
-console.log('hi', myName);
+function firstNonRepeatingLetter(s) {
+
+    s.filter(a => a)
+    return s.length > 2;
+}
+console.log(firstNonRepeatingLetter('stress'));
